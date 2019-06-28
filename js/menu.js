db670dbf82a97c6d2eae66da14686b5e8816e516
ebisLab <ebaiden@techie.com>
2019-06-28 06:45:03 -0400

    button links

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,7 @@ window.onscroll = () => {
     const header = document.querySelector('#header1');
     if(this.scrollY <= 10) header.className = ''; else header.className = 'scroll';
   };
+
+  function redirectToURL(){
+    console.log('Hello yall')
+        document.querySelector('#button1').style.background='yellow'}
