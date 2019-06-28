b01a4e8e3bc2e64d6fb950967b6771ea1420c284
ebisLab <ebaiden@techie.com>
2019-06-28 07:26:04 -0400

    connecting links

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,9 @@
 
+const btn = document.querySelector('.dropdown');
 
-window.onscroll = () => {
+
+  window.onscroll = () => {
     console.log('Hello')
     const header = document.querySelector('#header1');
     if(this.scrollY <= 10) header.className = ''; else header.className = 'scroll';
   };
-
-  function redirectToURL(){
-    console.log('Hello yall')
-        document.querySelector('#button1').style.background='yellow'}
