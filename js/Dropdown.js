b92fa77b7473154a99c1ff043dc77f13f4e11ac9
ebisLab <ebaiden@techie.com>
2019-06-28 10:43:44 -0400

    login button placed to the right of the header

--- js/Dropdown.js
+++ js/Dropdown.js
@@ -1,26 +1,4 @@
-console.log('YOOOOO')
-
-// class Dropdown {
-  
-//     constructor(element) {
-
-//       this.element = element;
-//   this.button = element.querySelector('.dropdown-button');      
-//   this.content = element.querySelector('.dropdown-content');      
-//    this.button.addEventListener('click', this.toggleContent.bind(this))
-//     }
-  
-//     toggleContent() {
-//       console.log('toggle')
-//       this.content.classList.toggle('dropdown-hidden');
-//       console.log(this.content.classList)
-//       // console.log('Hello???')
-//     }
-//   }
-  
-  
-//   // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-//   let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+console.log('Drop Down section Works! Woohoo! ')
 
 
   window.onload=function(){
@@ -42,14 +20,6 @@ console.log('YOOOOO')
     
       let dropdowns = document.querySelectorAll('.dropdown').forEach(dropdown => new Dropdown(dropdown));
 
-      
-
-    // let btnddc = document.querySelector('.dropdown-button');
-    // let ddc=document.querySelector('.dropdown-content');
-    
-    // btnddc.addEventListener('click', function(){
-    //      ddc.classList.toggle('dropdown-hidden')
-    // })
   }
 
 
